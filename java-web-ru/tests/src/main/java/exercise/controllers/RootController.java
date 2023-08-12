bddd538bfee88fed9ff74e1fc50808a5df848ef8
@hexlet/cli <support@hexlet.io>
2023-08-12 16:58:18 +0300

    submit java-web-ru/tests

---
+++ java-web-ru/tests/src/main/java/exercise/controllers/RootController.java
@@ -0,0 +1,10 @@
+package exercise.controller;
+
+import io.javalin.http.Handler;
+
+public final class RootController {
+
+    public static Handler welcome = ctx -> {
+        ctx.render("index.html");
+    };
+}
---
+++ java-web-ru/tests/src/test/java/exercise/AppTest.java
@@ -0,0 +1,139 @@
+package exercise;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import io.javalin.Javalin;
+import io.ebean.DB;
+
+import exercise.domain.User;
+import exercise.domain.query.QUser;
+import io.ebean.Database;
+
+class AppTest {
+
+    private static Javalin app;
+    private static String baseUrl;
+
+    // BEGIN
+    @BeforeAll
+    public static void beforeAll() {
+        // Получаем инстанс приложения
+        app = App.getApp();
+        // Запускаем приложение на рандомном порту
+        app.start(0);
+        // Получаем порт, на которм запустилось приложение
+        int port = app.port();
+        // Формируем базовый URL
+        baseUrl = "http://localhost:" + port;
+    }
+    @AfterAll
+    public static void afterAll() {
+        // Останавливаем приложение
+        app.stop();
+    }
+
+    // END
+
+    // Между тестами база данных очищается
+    // Благодаря этому тесты не влияют друг на друга
+    @BeforeEach
+    void beforeEach() {
+        Database db = DB.getDefault();
+        db.truncate("users");
+        User existingUser = new User("Wendell", "Legros", "a@a.com", "123456");
+        existingUser.save();
+    }
+
+    @Test
+    void testUsers() {
+
+        // Выполняем GET запрос на адрес http://localhost:port/users
+        HttpResponse<String> response = Unirest
+            .get(baseUrl + "/users")
+            .asString();
+        // Получаем тело ответа
+        String content = response.getBody();
+
+        // Проверяем код ответа
+        assertThat(response.getStatus()).isEqualTo(200);
+        // Проверяем, что страница содержит определенный текст
+        assertThat(response.getBody()).contains("Wendell Legros");
+    }
+
+    @Test
+    void testNewUser() {
+
+        HttpResponse<String> response = Unirest
+            .get(baseUrl + "/users/new")
+            .asString();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+
+    // BEGIN
+    void testCreateUser() {
+
+        // Выполняем POST запрос при помощи агента Unirest
+        HttpResponse<String> responsePost = Unirest
+                // POST запрос на URL
+                .post(baseUrl + "/users")
+                // Устанавливаем значения полей
+                .field("firstName", "John")
+                .field("lastName", "Doe")
+                .field("email", "john@oe.com")
+                .field("password", "JohnDoe")
+                // Выполняем запрос и получаем тело ответ с телом в виде строки
+                .asString();
+
+        // Проверяем статус ответа
+        assertThat(responsePost.getStatus()).isEqualTo(302);
+
+        // Проверяем, что компания добавлена в БД
+        User actualUser = new QUser()
+                .firstName.equalTo("John")
+                .findOne();
+
+        assertThat(actualUser).isNotNull();
+
+        // И что её данные соответствуют переданным
+        assertThat(actualUser.getFirstName()).isEqualTo("John");
+        assertThat(actualUser.getLastName()).isEqualTo("Doe");
+        assertThat(actualUser.getEmail()).isEqualTo("john@oe.com");
+        assertThat(actualUser.getPassword()).isEqualTo("JohnDoe");
+    }
+
+    @Test
+    void testCreateInvalidUser() {
+
+        HttpResponse<String> responsePost = Unirest
+                // POST запрос на URL
+                .post(baseUrl + "/users")
+                // Устанавливаем значения полей
+                .field("firstName", "Lloyd")
+                // Выполняем запрос и получаем тело ответ с телом в виде строки
+                .asString();
+
+        // Проверяем статус ответа
+        assertThat(responsePost.getStatus()).isEqualTo(422);
+
+        User actualUser = new QUser()
+                .firstName.equalTo("Lloyd")
+                .findOne();
+
+        assertThat(actualUser).isNull();
+
+        // Так можно получить содержимое тела ответа
+        String content = responsePost.getBody();
+
+        // И проверить, что оно содержит определённую строку
+        assertThat(content).contains("NULLCHECK_FAILED");
+    }
+    // END
+}
