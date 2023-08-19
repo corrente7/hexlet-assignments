d2763bbdf20bab3d9a708fb338982042721e02c3
@hexlet/cli <support@hexlet.io>
2023-08-19 15:08:08 +0300

    submit java-web-ru/javalin-intro

---
+++ java-web-ru/javalin-intro/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    application
+    id("com.adarshr.test-logger") version "3.2.0"
+}
+
+application {
+    mainClass.set("exercise.App")
+}
+
+dependencies {
+    implementation("io.javalin:javalin:5.6.1")
+}
+group = "exercise"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // BEGIN
+    
+    // END
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.konghq:unirest-java:3.13.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+testlogger {
+  showStandardStreams = true
+}
