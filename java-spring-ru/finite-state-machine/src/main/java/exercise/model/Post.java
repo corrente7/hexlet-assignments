b599b48f0282876da2ea6d552aceeacf6e8873e2
@hexlet/cli <support@hexlet.io>
2023-09-17 23:49:48 +0300

    submit java-spring-ru/finite-state-machine

---
+++ java-spring-ru/finite-state-machine/src/main/java/exercise/model/Post.java
@@ -0,0 +1,55 @@
+package exercise.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.GenerationType;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String title;
+
+    @Lob
+    private String body;
+
+    private PostState state = PostState.CREATED;
+
+    // BEGIN
+    public boolean publish() {
+        // Если переход возможен
+        if (state == PostState.CREATED) {
+            // Меняем состояние
+            state = PostState.PUBLISHED;
+            return true;
+        }
+        // Если переход не возможен
+        return false;
+    }
+    public boolean archive() {
+        // Если переход возможен
+        if (state == PostState.CREATED || state == PostState.PUBLISHED) {
+            // Меняем состояние
+            state = PostState.ARCHIVED;
+            return true;
+        }
+        // Если переход не возможен
+        return false;
+    }
+
+    // END
+}
